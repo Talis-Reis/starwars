07048a8c84846d2d77e11e5b5e97f4f9d80fdb2f
maiconwps <maiconobmep@hotmail.com>
2022-03-13 15:21:51 -0400

    feat: inicia negociaçãoe entre rebeldes.

---
+++ src/main/java/br/com/letscode/starwars/controller/NegotiationsRestController.java
@@ -0,0 +1,30 @@
+package br.com.letscode.starwars.controller;
+
+import br.com.letscode.starwars.model.DTO.StartNegotiationRequest;
+import br.com.letscode.starwars.model.DTO.StartedNegotiationResponse;
+import br.com.letscode.starwars.service.NegotiationService;
+import br.com.letscode.starwars.service.RebelsService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/rebels/{id}/negotiations")
+@Tag(name="Negotiations")
+@AllArgsConstructor
+@Slf4j
+public class NegotiationsRestController {
+    private final RebelsService rebelsService;
+    private final NegotiationService negotiationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public StartedNegotiationResponse createNegotiation(@PathVariable("id") Long id, @RequestBody StartNegotiationRequest request){
+        log.info("Creating  negotiation: {}", request);
+        var negotiation =  negotiationService.startNew(id, request);
+        log.info("Negotiation Created: {}", negotiation);
+        return negotiation;
+    }
+}
---
+++ src/main/java/br/com/letscode/starwars/model/DTO/AcceptedNegotiationResponse.java
@@ -0,0 +1,4 @@
+package br.com.letscode.starwars.model.DTO;
+
+public class AcceptedNegotiationResponse {
+}
