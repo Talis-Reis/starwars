81ebb402dcc2a423bdb0fa0d9ee96c6ad70537f0
Talis Aparecido dos Reis <talisapreis@gmail.com>
2022-03-13 23:34:04 -0300

    Validation Report

--- src/main/java/br/com/letscode/starwars/enums/errors/ReportValidationError.java
+++ src/main/java/br/com/letscode/starwars/enums/errors/ReportValidationError.java
@@ -4,9 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public enum ReportValidationError implements ValidationError{
     REBELS_CAN_ONLY_REPORT_ONCE(HttpStatus.BAD_REQUEST),
-    THIS_REBEL_IS_A_TRAITOR(HttpStatus.BAD_REQUEST),
-    THIS_REBEL_A_TRAITOR(HttpStatus.BAD_REQUEST),
-    THIS__IS_A_TRAITOR(HttpStatus.BAD_REQUEST);
+    THIS_REBEL_IS_A_TRAITOR(HttpStatus.BAD_REQUEST);
 
     private final HttpStatus status;
 
